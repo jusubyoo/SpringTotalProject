7a7b9abf67a09dbe1ff87432b3a8a5dd3dcda380
jusubyoo <dbwntjq1998@gmail.com>
2026-01-29 13:34:57 +0900

    1234

---
+++ src/main/java/com/sist/web/service/FoodService.java
@@ -0,0 +1,17 @@
+
+package com.sist.web.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.sist.web.vo.FoodVO;
+
+public interface FoodService {
+		public List<FoodVO> foodListData(int start);
+		public int foodTotalPage();
+		public List<FoodVO> foodFindData(int start,String address );
+		public int foodFindTotalPage(String address);
+		public FoodVO foodDetailData(int fno);
+}
